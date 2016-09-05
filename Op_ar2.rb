71de46979512c78898486af673251bf19b2daa2a
gil <gil@MacBook-Pro-de-gil.local>
2016-09-05 17:36:38 -0600

    op_ar2

---
+++ Op_ar2.rb
@@ -0,0 +1,12 @@
+suma = 2 + 2
+resta = 10 - 6
+p suma == resta 
+# => true
+
+var_1 = 20
+var_2 = 10
+resta = var_1 - var_2
+multiplicacion = resta * (var_1 + var_2)
+division = multiplicacion / 3
+p division == 100
+#=> true
